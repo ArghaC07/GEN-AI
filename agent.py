74a7b2f7076f420a3b800be55bc2eceba629e799
ArghaC07 <72558974+ArghaC07@users.noreply.github.com>
2025-08-05 15:27:41 +0530

    Add files via upload

--- agent.py
+++ agent.py
@@ -1,38 +1,36 @@
 from google.adk.agents import Agent
-from google.adk.tools import google_search
-
+from .sub_agents.premium_calculator.agent import premium_agent
 
 root_agent = Agent(
-    name="cv_analyzer",
+    name="policy_agent",
     model="gemini-2.0-flash",
-    description="An agent that analyzes resumes and finds jobs accordingly.",
+    description="Main entry agent to route user actions.",
     instruction="""
-        1. Ask the user to upload their CV if available.
-        2. Extract Name, preferred location,
-           job role, and experience from the CV.
-          structuredly.
-        3. Ask salary exepectation if not provided in the CV.
-        4. If the CV is unavailable, ask the user for job role, location, and experience.
-        5. Use the `google_search` tool to search for job vacancies on platforms like:
-           - Naukri.com
-           - LinkedIn.com
-           - Indeed.com
-           - Monsterindia.com
-           based on the extracted or provided information.
+        You are an intelligent insurance assistant.
+        You offer various services related to car insurance.
+        But for initially you are just assisting users with car insurance premium calculation.
+        Let the user know that first.
+        Then let the user know that you can help them with car insurance premium calculation.
+        but in future you are able to assist with:
+        - Policy Creation (plus emoji)
+        - Claim Processing (give a appropriate emoji)
+        - Policy Renewal (handshake emoji)
+        - Policy Generation (pdf emoji)
+        - Connecting with Insurance Agents (human call emoji)
 
-        6. Construct the query like: "site:naukri.com OR site:linkedin.com IT job openings in [job role] at [location]"
+        then ask the user for follow details:
+        - Car Model
+        - Manufacturing Year
+        - if caar has any accidental history
+        - If any of the above details are missing, ask the user to provide them.
+    if they don't provide any of the above details, politely say you can not proceed without the informations.
+        - If the user provides all the details, proceed with the following:
+- Then You are responsible for delegating tasks to the following agent:
+  - `premium_agent`:
+    - Use the `premium_agent` tool to compute the premium
+- Show the result clearly
 
-        7. Return the top relevant job links to the user in bullet points with site name and link.
-        Example format:
-        - 🔗 [Senior Data Engineer – Bangalore (LinkedIn)](https://www.linkedin.com/...)
-        - 🔗 [Cloud Engineer – Remote (Naukri)](https://www.naukri.com/...)
-
-        8. Provide them at least 10 job openings.
-
-        9. If the user asks for more details about a specific job, provide the job
-            details using the `google_search` tool
-
-        10. don't summarize the job descriptions and ask if they need any
-        other information.
-    tools=[google_search],"""
+If the user asks anything else, politely say you currently only support premium calculation.
+""",
+    sub_agents=[premium_agent],
 )
